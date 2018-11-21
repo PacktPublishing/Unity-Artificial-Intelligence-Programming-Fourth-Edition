e2255316b6eaa1f572c602f8fe6aeab02f35edd1
Davide Aversa <thek3nger@gmail.com>
2018-11-21 16:38:00 +0100

    CH08 - Update to Unity 2018

--- 3400_08_codes/Assets/Scripts/Target.cs
+++ 3400_08_codes/Assets/Scripts/Target.cs
@@ -3,17 +3,17 @@ using System.Collections;
 
 public class Target : MonoBehaviour
 {
-    private NavMeshAgent[] navAgents;
+    private UnityEngine.AI.NavMeshAgent[] navAgents;
     public Transform targetMarker;
 
     void Start ()
     {
-	    navAgents = FindObjectsOfType(typeof(NavMeshAgent)) as NavMeshAgent[];
+	    navAgents = FindObjectsOfType(typeof(UnityEngine.AI.NavMeshAgent)) as UnityEngine.AI.NavMeshAgent[];
     }
 
     void UpdateTargets ( Vector3 targetPosition  )
     {
-	    foreach(NavMeshAgent agent in navAgents) 
+	    foreach(UnityEngine.AI.NavMeshAgent agent in navAgents) 
         {
 		    agent.destination = targetPosition;
 	    }
